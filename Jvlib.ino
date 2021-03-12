b750d311fc90029a3778645165f932cd16b0af9f
scord <70288480+scordd@users.noreply.github.com>
2021-03-12 11:02:31 -0800

    Add files via upload

--- Jvlib.ino
+++ Jvlib.ino
@@ -1,15 +1,20 @@
 #include "Jvlib.h"
+#include "Servo.h"
 #include <CrcLib.h>
 using namespace Crc;
 
-
+Servo Motor;
+const int MF = 20;
+const int MB = 138;
+const int MS = 91;
 
 void setup() {
   // put your setup code here, to run once:
   CrcLib::Initialize();
-
-  // other 2 motors (for high-torque control of the arm)
-  CrcLib::InitializePwmOutput(CRC_PWM_6);
+  Serial.begin(9600);
+  Motor.attach(7); // connects to PWM pin 7
+  
+  // 1 motor (for high-torque control of the arm)
   CrcLib::InitializePwmOutput(CRC_PWM_7);
 
   // set of 4 motors that will be used for moving the entire robot
@@ -20,6 +25,20 @@ void setup() {
 
   CrcLib::Update();
 
+  // CODE BELOW IS FOR THE ARM OF THE BOT
+  Motor.write(MF);
+  delay(1000);
+
+  Motor.write(MS);
+  delay(1000);
+
+  Motor.write(MB);
+  delay(1000);
+
+  Motor.write(MS);
+  delay(1000);
+  
+  // CODE BELOW IS FOR THE MOVEMENT OF THE BOT
   forward();
   back();
 
