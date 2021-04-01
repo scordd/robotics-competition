20330991d36296a63decabf321d2abb98b169551
Jackette <64053033+Jackette-tech@users.noreply.github.com>
2021-04-01 13:21:00 -0700

    Major update (v2)

--- Jvlib.ino
+++ Jvlib.ino
@@ -1,14 +1,29 @@
-#include "Jvlib.h"
 #include <CrcLib.h>
 using namespace Crc;
+
+#define PULLY CRC_PWM_6
+#define SERVO CRC_PWM_7
+#define BL CRC_PWM_11
+#define BR CRC_PWM_9
+#define FL CRC_PWM_10
+#define FR CRC_PWM_8
+
+
+int forwardTime = 0;
+int backwardTime = 0;
+int servoCloseTime = 0;
+int servoOpenTime = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
   CrcLib::Initialize();
   Serial.begin(9600);
 
+  // 1 pully motor (arm)
+  CrcLib::InitializePwmOutput(CRC_PWM_6);
   
-  // 1 motor (for high-torque control of the arm)
+  // 1 servo (for high-torque control of the arm)
   CrcLib::InitializePwmOutput(CRC_PWM_7);
 
   // set of 4 motors that will be used for moving the entire robot
@@ -17,17 +32,37 @@ void setup() {
   CrcLib::InitializePwmOutput(CRC_PWM_10);
   CrcLib::InitializePwmOutput(CRC_PWM_11);
 
-  CrcLib::Update();
-
-  // CODE BELOW IS FOR THE ARM OF THE BOT
-  servo();
-  
-  // CODE BELOW IS FOR THE MOVEMENT OF THE BOT
-  forward();
-  back();
 }
 
 void loop() {
   CrcLib::Update();
+
+
+  // MOVE EVERYTHING TO VOID SETUP() ONCE SCRIPTING HAS BEGUN
+
+
+  // CODE BELOW IS FOR THE MOVEMENT OF THE BOT (forward)
+
+  CrcLib::Initialize();
+
+  CrcLib::SetPwmOutput(CRC_PWM_10, -100);
+  CrcLib::SetPwmOutput(CRC_PWM_11, -100);
+  CrcLib::SetPwmOutput(CRC_PWM_8, 100);
+  CrcLib::SetPwmOutput(CRC_PWM_9, 100);
+
+  // CODE BELOW IS FOR THE MOVEMENT OF THE BOT (backward)
+
+  CrcLib::Initialize();
+
+  CrcLib::SetPwmOutput(CRC_PWM_10, 100);
+  CrcLib::SetPwmOutput(CRC_PWM_11, 100);
+  CrcLib::SetPwmOutput(CRC_PWM_8, -100);
+  CrcLib::SetPwmOutput(CRC_PWM_9, -100);
+
+  // CODE BELOW IS FOR THE ARM OF THE BOT (Open)
+  
+  // CODE BELOW IS FOR THE ARM OF THE BOT (Close)
+
+  // CODE BELOW IS FOR THE PULLY OF THE ARM
 
 }
