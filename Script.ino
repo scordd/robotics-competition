37fe132812ca5ee8930532d6fc5c91ee74cb6b6b
scord <70288480+scordd@users.noreply.github.com>
2021-04-06 09:29:36 -0700

    Update Script.ino

--- Script.ino
+++ Script.ino
@@ -46,5 +46,236 @@ void setup() {
 }
 
 void loop() {
-  CrcLib::Update();
-}
+  ButtonState = CrcLib::GetDigitalInput(CRC_DIG_5);
+
+  if (ButtonState = HIGH){
+      left(5000);
+      forward(50000);
+      backward(50000);
+      right(50000);
+      
+      CrcLib::Update();
+    } else {
+      CrcLib::Update();
+    }
+};
+
+
+int forward(int t) // moving robot forwards
+{
+  for (int i = 0; i < t; i++) {
+
+    CrcLib::SetPwmOutput(CRC_PWM_8, 75);
+    CrcLib::SetPwmOutput(CRC_PWM_9, -75);
+    CrcLib::SetPwmOutput(CRC_PWM_10, 75);
+    CrcLib::SetPwmOutput(CRC_PWM_11, -75);
+
+    CrcLib::Update();
+  }
+  return 0;
+};
+
+int forwardpressure(int t) // moving robot forwards
+{
+  for (int i = 0; i < t; i++) {
+
+    CrcLib::SetPwmOutput(CRC_PWM_8, 75);
+    CrcLib::SetPwmOutput(CRC_PWM_9, -75);
+    CrcLib::SetPwmOutput(CRC_PWM_10, 75);
+    CrcLib::SetPwmOutput(CRC_PWM_11, -75);
+
+    CrcLib::SetPwmOutput(CRC_PWM_6, -25);
+    CrcLib::SetPwmOutput(CRC_PWM_7, 25);
+
+    CrcLib::Update();
+  }
+  return 0;
+};
+
+int backward(int t) // moving robot backwards
+{
+  for (int i = 0; i < t; i++) {
+
+    CrcLib::SetPwmOutput(CRC_PWM_8, -75);
+    CrcLib::SetPwmOutput(CRC_PWM_9, 75);
+    CrcLib::SetPwmOutput(CRC_PWM_10, -75);
+    CrcLib::SetPwmOutput(CRC_PWM_11, 75);
+
+    CrcLib::Update();
+  }
+  return 0;
+};
+
+int backwardpressure(int t) // moving robot forwards
+{
+  for (int i = 0; i < t; i++) {
+
+    CrcLib::SetPwmOutput(CRC_PWM_8, 75);
+    CrcLib::SetPwmOutput(CRC_PWM_9, -75);
+    CrcLib::SetPwmOutput(CRC_PWM_10, 75);
+    CrcLib::SetPwmOutput(CRC_PWM_11, -75);
+
+    CrcLib::SetPwmOutput(CRC_PWM_6, -25);
+    CrcLib::SetPwmOutput(CRC_PWM_7, 25);
+
+    CrcLib::Update();
+  }
+  return 0;
+};
+
+int right(int t) // turning towards the right
+{
+  for (int i = 0; i < t; i++) {
+
+    CrcLib::SetPwmOutput(CRC_PWM_8, 50);
+    CrcLib::SetPwmOutput(CRC_PWM_9, 50);
+    CrcLib::SetPwmOutput(CRC_PWM_10, 50);
+    CrcLib::SetPwmOutput(CRC_PWM_11, 50);
+
+    CrcLib::Update();
+  }
+  return 0;
+};
+
+int rightpressure(int t) // turning towards the right
+{
+  for (int i = 0; i < t; i++) {
+
+    CrcLib::SetPwmOutput(CRC_PWM_8, 50);
+    CrcLib::SetPwmOutput(CRC_PWM_9, 50);
+    CrcLib::SetPwmOutput(CRC_PWM_10, 50);
+    CrcLib::SetPwmOutput(CRC_PWM_11, 50);
+
+    CrcLib::SetPwmOutput(CRC_PWM_6, -25);
+    CrcLib::SetPwmOutput(CRC_PWM_7, 25);
+
+    CrcLib::Update();
+  }
+  return 0;
+};
+int left(int t) // turning towards the left
+{
+  for (int i = 0; i < t; i++) {
+
+    CrcLib::SetPwmOutput(CRC_PWM_8, -50);
+    CrcLib::SetPwmOutput(CRC_PWM_9, -50);
+    CrcLib::SetPwmOutput(CRC_PWM_10, -50);
+    CrcLib::SetPwmOutput(CRC_PWM_11, -50);
+
+    CrcLib::Update();
+  }
+  return 0;
+};
+
+
+int leftpressure(int t) // turning towards the right
+{
+  for (int i = 0; i < t; i++) {
+
+    CrcLib::SetPwmOutput(CRC_PWM_8, 50);
+    CrcLib::SetPwmOutput(CRC_PWM_9, 50);
+    CrcLib::SetPwmOutput(CRC_PWM_10, 50);
+    CrcLib::SetPwmOutput(CRC_PWM_11, 50);
+
+    CrcLib::SetPwmOutput(CRC_PWM_6, -25);
+    CrcLib::SetPwmOutput(CRC_PWM_7, 25);
+
+    CrcLib::Update();
+  }
+  return 0;
+};
+
+int up(int t) // for vertical motor lifting the C.U.M.
+{
+  for (int i = 0; i < t; i++) {
+
+    CrcLib::SetPwmOutput(CRC_PWM_5, 50);
+
+    CrcLib::Update();
+  }
+  return 0;
+};
+
+int uppressure(int t) // for vertical motor lifting the C.U.M.
+{
+  for (int i = 0; i < t; i++) {
+
+    CrcLib::SetPwmOutput(CRC_PWM_5, 50);
+
+//    too keep presure on the object
+    CrcLib::SetPwmOutput(CRC_PWM_6, -25);
+    CrcLib::SetPwmOutput(CRC_PWM_7, 25);
+
+    CrcLib::Update();
+  }
+  return 0;
+};
+
+int down(int t) // for vertical motor lowering the C.U.M
+{
+  for (int i = 0; i < t; i++) {
+
+    CrcLib::SetPwmOutput(CRC_PWM_5, -50);
+
+    CrcLib::Update();
+  }
+  return 0;
+};
+
+int downpressure(int t) // for vertical motor lifting the C.U.M.
+{
+  for (int i = 0; i < t; i++) {
+
+    CrcLib::SetPwmOutput(CRC_PWM_5, 50);
+
+//    too keep presure on the object
+    CrcLib::SetPwmOutput(CRC_PWM_6, -25);
+    CrcLib::SetPwmOutput(CRC_PWM_7, 25);
+
+    CrcLib::Update();
+  }
+  return 0;
+};
+
+int grab(int t) // for horizontal motors to grab object
+{
+  for (int i = 0; i < t; i++) {
+
+    CrcLib::SetPwmOutput(CRC_PWM_6, -50);
+    CrcLib::SetPwmOutput(CRC_PWM_7, 50);
+
+    CrcLib::Update();
+  }
+  return 0;
+};
+
+int drop(int t) // for horizontal motors to grab object
+{
+  for (int i = 0; i < t; i++) {
+
+    CrcLib::SetPwmOutput(CRC_PWM_6, 50);
+    CrcLib::SetPwmOutput(CRC_PWM_7, -50);
+
+    CrcLib::Update();
+  }
+  return 0;
+};
+
+int done(int t) // turning towards the right
+{
+  for (int i = 0; i < t; i++) {
+
+    CrcLib::SetPwmOutput(CRC_PWM_8, 0);
+    CrcLib::SetPwmOutput(CRC_PWM_9, 0);
+    CrcLib::SetPwmOutput(CRC_PWM_10, 0);
+    CrcLib::SetPwmOutput(CRC_PWM_11, 0);
+
+    CrcLib::SetPwmOutput(CRC_PWM_6, 0);
+    CrcLib::SetPwmOutput(CRC_PWM_7, 0);
+
+    CrcLib::SetPwmOutput(CRC_PWM_5, 0);
+
+    CrcLib::Update();
+  }
+  return 0;
+};
